9c45f01c4a33cbb055f783bcc31c3f512b423ef5
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-09-16 21:49:39 +0200

    feat(server): migrate from Node.js HTTP to Express
    Refactored the application to use Express for routing and serving HTML files instead of the built-in Node.js HTTP module. This simplifies route handling and improves maintainability.

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-import http from "http";
+import express from "express";
 import fs from "fs";
 
-// Create a server that listens for requests
-const server = http.createServer((req, res) => {
-    // Add the type of data the server will respond with, in the response's header
-    res.setHeader("Content-Type", "text/html");
+const app = express();
 
-    // Set up which file the serve will respond with for each requested url
-    let file;
-    switch (req.url) {
-        case "/":
-            file = "index.html";
-            res.statusCode = 200;
-            break;
-        case "/about":
-            file = "about.html";
-            res.statusCode = 200;
-            break;
-        case "/contact-me":
-            file = "contact-me.html";
-            res.statusCode = 200;
-            break;
-        default:
-            file = "404.html";
-            res.statusCode = 404;
-            break;
-    }
-
-    // Read the data on the file that the server will respond with
-    fs.readFile(file, (err, data) => {
+app.get("/", (req, res) => {
+    res.set("Content-Type", "text/html");
+    fs.readFile("index.html", (err, data) => {
         if (err) {
             console.log(err);
+            res.status(500).end();
         } else {
-            // Add the file's data to the response's body
-            res.end(data);
+            res.status(200).end(data);
         }
     });
 });
 
-server.listen(8080, "localhost", () => {
+app.get("/about", (req, res) => {
+    res.set("Content-Type", "text/html");
+    fs.readFile("about.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.status(200).end(data);
+        }
+    });
+});
+
+app.get("/contact-me", (req, res) => {
+    res.set("Content-Type", "text/html");
+    fs.readFile("contact-me.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.status(200).end(data);
+        }
+    });
+});
+
+app.use((req, res) => {
+    res.set("Content-Type", "text/html");
+    fs.readFile("404.html", (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        } else {
+            res.status(404).end(data);
+        }
+    });
+});
+
+app.listen(8080, "localhost", () => {
     console.log("Server is running on port 8080");
 });
